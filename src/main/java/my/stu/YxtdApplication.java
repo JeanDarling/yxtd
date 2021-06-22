496ab206f2f69814b7b42a7378a85f4d1ace06c4
Jean <JeanDarling@163.com>
2021-06-22 09:42:51 +0800

    创建项目

---
+++ src/main/java/my/stu/YxtdApplication.java
@@ -0,0 +1,13 @@
+package my.stu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxtdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxtdApplication.class, args);
+    }
+
+}
